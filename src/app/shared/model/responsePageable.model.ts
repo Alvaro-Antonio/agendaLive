f33acebf1635f2f2f21b828044ed6cebe2521ac0
Alvaro-Antonio <alvaro.antonio@dcx.ufpb.br>
2021-01-20 12:11:49 -0300

    fist service

---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,13 @@
+export class ResponsePageable{
+  content: any[];
+  fist:boolean;
+  last:boolean;
+  number:number;
+  numberOfElements: number;
+  pageable: any[];
+  size: number;
+  sort: number;
+  totalElements:number;
+  totalPages: number;
+
+}
